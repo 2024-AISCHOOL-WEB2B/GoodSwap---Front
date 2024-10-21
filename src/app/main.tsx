6dacc3aae728568846297c6c4f981a4a83b70df0
HyeseongRo <nks3319@gmail.com>
2024-10-22 08:52:50 +0900

    chore: 일단 수정 커밋

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,8 +3,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "jotai"; // Jotai Provider 추가
-import App from "./App.tsx";
-import "./index.css";
+import App from "./App";
+import "/src/shared/styles/index.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
