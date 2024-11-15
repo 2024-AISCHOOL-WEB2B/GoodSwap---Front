00170d258d0017a7557c668a394397e29a77ffcc
Park-Ji-Gwang <wl5639@naver.com>
2024-11-15 11:08:13 +0900

    다시한번커밋합니다

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,18 +1,25 @@
 // src/app/main.tsx
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "jotai"; // Jotai Provider 추가
-import App from "./App";
-import "../shared/styles/index.css"; // 상대 경로로 수정
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'jotai'; // Jotai Provider 추가
+import App from './App';
+import '../shared/styles/index.css'; // 상대 경로로 수정
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <Provider>
-        <App />
-      </Provider>
-    </StrictMode>
-  );
+// Mock 설정 임포트
+if (process.env.NODE_ENV === 'development') {
+    // import("../features/auth/mocks/axiosMock"); // 개발 환경에서만 Mock 설정 적용
 }
+
+// HTML 파일에서 "root"라는 ID를 가진 요소를 가져옴 -- if문을 제거해서 에러가 발생하게 변경
+const rootElement = document.getElementById('root');
+createRoot(rootElement!).render(
+    // "!" 타입 가드용어
+    <StrictMode>
+        {/* Jotai의 전역 상태 관리 제공 */}
+        <Provider>
+            {/* App 컴포넌트를 렌더링 */}
+            <App />
+        </Provider>
+    </StrictMode>
+);
